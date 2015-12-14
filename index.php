7fa8db03953790f35fd69c11746556cf99cc049f
Viktoriya Bilyk <viktoriya566@rambler.ru>
2015-12-14 13:28:57 +0300

    добавлены стили для главного меню

--- index.php
+++ index.php
@@ -8,12 +8,20 @@
 	<script type="text/javascript" src="js/main.js"></script>
 </head>
 <body>
-	<a href="create.php">Создать базу данных</a>
-	<a href="clients/clients_get.php">Клиенты</a>
-	<a href="suppliers/suppliers_get.php">Поставщики</a>
-	<a href="banks/banks_get.php">Банки</a>
-	<a href="order/order_get.php">Заказ-наряд</a>
-	<a href="lease_agreement/lease_agreement_get.php">Договор лизинга</a>
-	<a href="crediting/crediting_get.php">Кредитование</a>
+	<!-- <a href="create.php">Создать базу данных</a> -->
+<nav class="menu-navigation">
+	<div class="menu-navigation__left">
+		<a class="menu-navigation__item" href="clients/clients_get.php">Клиенты</a>
+		<a class="menu-navigation__item" href="suppliers/suppliers_get.php">Поставщики</a>
+		<a class="menu-navigation__item" href="banks/banks_get.php">Банки</a>
+		<a class="menu-navigation__item" href="payments/payments_get.php">Контроль платежей</a>
+	</div>
+	<div class="menu-navigation__right">
+		<a class="menu-navigation__item" href="order/order_get.php">Заказ-наряд</a>
+		<a class="menu-navigation__item" href="lease_agreement/lease_agreement_get.php">Договор лизинга</a>
+		<a class="menu-navigation__item" href="crediting/crediting_get.php">Кредитование</a>
+		<a class="menu-navigation__item" href="purchase_sale/purchase_sale_get.php">Купля-продажа</a>
+	</div>
+</nav>
 </body>
 </html>
